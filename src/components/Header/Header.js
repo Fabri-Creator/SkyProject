56dd00138f528ed658c39d8326e8d8f73e548bc4
Fabri-Lan <fabriciolancieri@gmail.com>
2020-11-14 02:02:43 +0100

    beginning

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="main-header">
+      <div className="sex-container">
+        <h3>Mujer</h3>
+        <h3>Hombre</h3>
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/Products.js
@@ -0,0 +1,12 @@
+// import RoomList from "../components/RoomList";
+import "./page.scss";
+
+const Products = () => {
+  return (
+    <div className="products-container">
+      <Category />
+    </div>
+  );
+};
+
+export default Products;
